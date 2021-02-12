21e937b447417dddbe7632c4701e2452690a465b
Clair <clairhoulihan@yahoo.com>
2021-02-12 13:32:59 -0600

    getFiles uploaded

---
+++ project1/getFiles.bash
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-000000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-010000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-020000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-030000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-040000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-050000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-060000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-070000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-080000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-090000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-100000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-110000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-120000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-130000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-140000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-150000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-160000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-170000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-180000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-190000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-200000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-210000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-220000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2020/2020-06/pageviews-20200601-230000.gz
+
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-000000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-010000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-020000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-030000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-040000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-050000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-060000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-070000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-080000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-090000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-100000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-110000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-120000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-130000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-140000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-150000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-160000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-170000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-180000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-190000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-200000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-210000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-220000.gz
+wget https://dumps.wikimedia.org/other/pageviews/2021/2021-01/pageviews-20210120-230000.gz
+
+wget https://dumps.wikimedia.org/other/clickstream/2020-06/clickstream-enwiki-2020-06.tsv.gz
+
+wget https://dumps.wikimedia.org/other/mediawiki_history/2020-12/enwiki/2020-12.enwiki.2020-06.tsv.bz2
